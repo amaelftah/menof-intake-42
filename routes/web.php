6f804cf298653b58b9eaf896c50aa615a6d92070
amabdelftah <ahmedabdelftah95165@gmail.com>
2022-04-13 09:22:46 +0200

    Add laravel/ui

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,7 @@ Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 Route::get('/posts/create/', [PostController::class, 'create'])->name('posts.create');
 Route::post('/posts',[PostController::class, 'store'])->name('posts.store');
 Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
